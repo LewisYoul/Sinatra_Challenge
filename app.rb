e119b62ea7a31c9032580d37007a29760d7819f7
Lewis <lewisyoul@gmail.com>
2017-11-07 12:11:47 +0000

    create index view and form

--- app.rb
+++ app.rb
@@ -8,8 +8,13 @@ get '/secret' do
   "SHHH, it's a secret!!!!"
 end
 
-get '/cat' do
-  '<style> img { border: 2px dashed; color: red; } </style>
+get '/random-cat' do
+  @name = ["Lewis", "Ignacio"].sample
+  erb(:index)
+end
 
-  <img src="http://bit.ly/1eze8aE">'
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
